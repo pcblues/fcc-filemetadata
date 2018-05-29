0dff9b9ac6183f0107e51539e7f50fa9cce7d705
Glitch (fcc-ffilemetadata) <none>
2018-05-29 19:10:51 +0000

    💨🌴 Updated with Glitch

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+var multer=require ('multer')
+
+
+
+                    
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -14,7 +19,14 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
-});
+})
+
+var upload=multer({storage:multer.memoryStorage()}).single('uploadfile')
+
+app.post('/upload', upload,function(req,res,next) {
+  console.log(req.file.size)
+  res.send({size:req.file.size})
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
